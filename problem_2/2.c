f511211c15e383093887b1614dbf786ba606f3fd
Hashaam Zafar <hashaam1217@gmail.com>
2024-03-31 04:09:41 -0700

    Checkpoint

--- problem_2/2.c
+++ problem_2/2.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 struct ListNode {
      int val;
      struct ListNode *next;
 };
+
+#define TRUE 1
+#define FALSE 0
  
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     int carryover = 0;
@@ -12,44 +16,55 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     result->val = 0;
     struct ListNode *HeadNode = result;
 
-    while (l1->next != NULL || l2->next != NULL) {
-        //Dealing with the case of uneven pointer lengths
-        if (l1->next != NULL)
+    while (TRUE) {
+        if (l1->next != NULL || l2->next != NULL)
         {
-            result->val += l1->val;
-            l1 = l1->next;
+            if (l1 != NULL)
+            {
+                result->val += l1->val;
+            }
+            if (l1->next != NULL)
+            {
+                l1 = l1->next;
+            }
+            if (l2 != NULL)
+            {
+                result->val += l2->val;
+            }
+            if (l2->next != NULL)
+            {
+                l2 = l2->next;
+            }
+            //Handling addition
+            result->val = carryover;
+            if (result->val >= 10) 
+            {
+                carryover = 1;
+                result->val -= 10;
+            }
+            else 
+            {
+                carryover = 0;
+            }
+            struct ListNode *nextNode = (struct ListNode*) malloc(sizeof(struct ListNode));
+            result->next = nextNode;
+            result = result->next;
         }
-        if (l2->next!=NULL)
+        else
         {
-            result->val += l2->val;
-            l2 = l2->next;
-        }
-        //Handling addition
-        result->val = carryover;
-        if (result->val >= 10) {
-            carryover = 1;
-            result->val -= 10;
-        }
-        else {
-            carryover = 0;
-        }
+            if (carryover == 1)
+            {
+                struct ListNode *nextNode = (struct ListNode*) malloc(sizeof(struct ListNode));
+                result->next = nextNode;
+                result = result->next;
+                result->val = 1;
 
-        //Handling moving result pointer along the list
-        struct ListNode *nextNode = (struct ListNode*) malloc(sizeof(struct ListNode));
-        result->next = nextNode;
-        result = result->next;
+            }
+            result->next = NULL;
+
+            return HeadNode;
+        }
     }
-    if (carryover == 1)
-    {
-        struct ListNode *nextNode = (struct ListNode*) malloc(sizeof(struct ListNode));
-        result->next = nextNode;
-        result = result->next;
-        result->val = 1;
-
-    }
-    result->next = NULL;
-
-    return HeadNode;
 }
 
 int main()
